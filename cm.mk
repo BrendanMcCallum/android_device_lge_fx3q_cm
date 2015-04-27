d92ea680d050d6a6ff9a8e6e022a87712b2c62a5
xdajog <mail@se-di.de>
2015-04-27 17:45:24 +0200

    Adaption to fx3q third part

--- cm.mk
+++ cm.mk
@@ -5,9 +5,11 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/lge/fx3q/fx3q.mk)
 
 #PRODUCT_BUILD_PROP_OVERRIDES +=
+# Release name
+PRODUCT_RELEASE_NAME := fx3q
 
 PRODUCT_DEVICE := fx3q
 PRODUCT_NAME := cm_fx3q
 PRODUCT_BRAND := LGE
 PRODUCT_MANUFACTURER := lge
-PRODUCT_MODEL := LGMS500
+PRODUCT_MODEL := F3Q
